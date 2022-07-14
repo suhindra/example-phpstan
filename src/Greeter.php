28efdbbeee9c8b327e0358eba275e35920cf802e
Suhindra <suhindra@hotmail.co.id>
2022-07-14 15:46:34 +0700

    Create Greeter.php

--- src/Greeter.php
+++ src/Greeter.php
@@ -10,9 +10,7 @@ class Greeter {
 		$rand1 = gmp_random(1); // random number from 0 to 1 * bits per limb
 		$rand2 = gmp_random(2); // random number from 0 to 2 * bits per limb
 
-		echo gmp_strval($rand1) . "\n";
-		echo gmp_strval($rand2) . "\n";
-
+		return gmp_strval($rand1) ;
 		
 	}
 }
